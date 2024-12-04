14df5a19bd671fb74db8d7c4e18eaac4f2395720
lcozdenm <lcozdenm@student.42.fr>
2024-12-04 17:28:09 +0100

    Day 02 done: 2 golden star

--- day02/day02.py
+++ day02/day02.py
@@ -1,10 +1,8 @@
 import sys
 
-def is_safe(line):
-    tab = line.split()
-    tab_step = [int(tab[i]) - int(tab[i + 1]) for i in range(len(tab) - 1)]
-    print(tab_step)
-    return (is_sorted(tab_step) and check_step(tab_step, 1, 3))
+def extract_steps(line_tab):
+    tab_step = [int(line_tab[i]) - int(line_tab[i + 1]) for i in range(len(line_tab) - 1)]
+    return (tab_step)
 
 def is_sorted(tab_step):
     return (
@@ -15,12 +13,39 @@ def is_sorted(tab_step):
         )
     )
 
-def check_step(tab_step, min, max):
+def check_step(tab_step, min = 1, max = 3):
     return (
         all(
             abs(step) >= min and abs(step) <= max for step in tab_step
         )
     )
+
+def is_safe(line_tab):
+    tab_step = extract_steps(line_tab)
+    return (is_sorted(tab_step) and check_step(tab_step))
+
+def is_dampener_safe(line_tab):
+    warning = 0
+    while not is_safe(line_tab):
+        warning += 1
+        if warning > 1:
+            return False
+        original_tab = line_tab.copy()
+        for i in range(len(original_tab)):
+            line_tab = original_tab.copy()
+            line_tab.pop(i)
+            if is_safe(line_tab):
+                return True
+        return False
+    return True
+
+def part_one(report):
+    safe_sum = sum([is_safe(line.split()) for line in report])
+    print(f"There is {safe_sum} reports that are safe")
+
+def part_two(report):
+    safe_sum = sum([is_dampener_safe(line.split()) for line in report])
+    print(f"There is {safe_sum} reports that are dampener safe")
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1):
@@ -28,7 +53,6 @@ if __name__ == "__main__":
     else:
         filename = "input"
     with open(filename, mode="r", encoding="utf-8") as file:
-        safe_sum = sum([is_safe(line) for line in file])
-        print(f"There is {safe_sum} reports that are safe")
-
-    
+        lines  = file.readlines()
+        part_one(lines)
+        part_two(lines)
